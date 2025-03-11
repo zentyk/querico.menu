cf8faf1902aba0d48452fa1ec76c5d578dc960cd
zentyk <zentyk@protonmail.com>
2025-03-10 21:35:44 -0600

    Added basic system

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let button = document.getElementById('platillo1');
+let showCartButton = document.getElementById('showCart');
+let closeCartButton = document.getElementById('close');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (button) {
+  button.addEventListener('click', () => {
+    console.log('agregado taco de birria') 
+  });
+}
+
+if (showCartButton) {
+  showCartButton.addEventListener('click', () => {
+    let modal = document.getElementById('modal');
+
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  });
+}
+
+if(closeCartButton) {
+  closeCartButton.addEventListener('click', () => {
+    let modal = document.getElementById('modal');
+
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
