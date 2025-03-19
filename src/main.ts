71566253da7d74dfbea811b00e0933cc62ae8313
zentyk <zentyk@protonmail.com>
2025-03-18 22:01:38 -0600

    Added final buttons

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,88 @@
 import './style.css';
 
 let button = document.getElementById('platillo1');
-let showCartButton = document.getElementById('ShowCart');
-let closeCartButton = document.getElementById('close');
 
+let showMapButton = document.getElementById('ShowMap');
+let showCartButton = document.getElementById('ShowCart');
+let showSocialButton = document.getElementById('ShowSocial');
+let showBankTagButton = document.getElementById('ShowBankTag');
+
+let closeCartButton = document.getElementById('closeCart');
+let closeSocialButton = document.getElementById('closeSocial');
+let closeBankTagButton = document.getElementById('closeBankTag');
+
+let modals = [
+  'modalMap',
+  'modalOrder',
+  'modalSocial',
+  'modalBankTag', 
+]
+
+//#region CartBehavior
 if (button) {
   button.addEventListener('click', () => {
     console.log('agregado taco de birria') 
   });
 }
+//#endregion
 
+//#region Modal BankTag
+if(showBankTagButton) {
+  showBankTagButton.addEventListener('click', () => {
+    let modal = document.getElementById(modals[3]);
+
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  });
+}
+
+if(closeBankTagButton) {
+  closeBankTagButton.addEventListener('click', () => {
+    let modal = document.getElementById(modals[3]);
+
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
+//#endregion
+
+//#region Modal Map
+if(showMapButton) {
+  showMapButton.addEventListener('click', () => {
+    //to google maps
+    window.open('https://maps.app.goo.gl/MV7Tepvimy9GDWtY6'); 
+  });
+}
+//#endregion
+
+//#region Modal Social
+if (showSocialButton) {
+  showSocialButton.addEventListener('click', () => {
+    let modal = document.getElementById(modals[2]);
+
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  });
+}
+
+if(closeSocialButton) {
+  closeSocialButton.addEventListener('click', () => {
+    let modal = document.getElementById(modals[2]);
+
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
+//#endregion
+
+//#region Modal Order
 if (showCartButton) {
   showCartButton.addEventListener('click', () => {
-    let modal = document.getElementById('modal');
+    let modal = document.getElementById(modals[1]);
 
     if (modal) {
       modal.style.display = 'block';
@@ -22,10 +92,11 @@ if (showCartButton) {
 
 if(closeCartButton) {
   closeCartButton.addEventListener('click', () => {
-    let modal = document.getElementById('modal');
+    let modal = document.getElementById(modals[1]);
 
     if (modal) {
       modal.style.display = 'none';
     }
   });
 }
+//#endregion
